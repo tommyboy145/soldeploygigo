62f5a19cfb7b87b52df9fe1b51ea69571ddb42ce
Tom Santos <tommysantos14@gmail.com>
2024-02-09 11:40:25 -0800

    a

--- src/components/slider/IntroSlider.js
+++ src/components/slider/IntroSlider.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import SwiperCore, { Autoplay, Navigation } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore, { Autoplay, Navigation } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
 SwiperCore.use([Autoplay, Navigation]);
 const IntroSlider = () => {
@@ -34,55 +34,7 @@ const IntroSlider = () => {
 
 
     return (
-        <>
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                loop={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false
-                }}
-                navigation={{
-                    prevEl: ".intro_prev",
-                    nextEl: ".intro_next",
-                }}
-                className="custom-className"
-            >
-                {data.map((item, i) => (
-                    <SwiperSlide>
-                        <div className="slider-item">
-                            <img
-                                src={`/images/items/${item.img}`}
-                                alt=""
-                                className="img-fluid"
-                            />
-                            {/* <div className="slider-item-avatar">
-                                <img
-                                    src={`/images/avatar/${item.avatar}`}
-                                    alt=""
-                                />
-                                <div>
-                                    <h5>{item.title}</h5>
-                                    <p>{item.author}</p>
-                                </div>
-                            </div> */}
-                        </div>
-                    </SwiperSlide>
-                ))}
-
-                <div className="arrows">
-                    <span className="intro_prev">
-                        <i className="bi bi-arrow-left"></i>
-                    </span>
-                    <span className="intro_next">
-                        <i className="bi bi-arrow-right"></i>
-                    </span>
-                </div>
-            </Swiper>
-
-
-        </>
+        <></>
     );
 };
 
